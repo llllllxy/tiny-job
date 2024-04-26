3fd8253265e84316422435ec9a3ad7968d4bd1c5
llllllxy <34022230+llllllxy@users.noreply.github.com>
2024-04-26 17:16:17 +0800

    Fixed: 完善SpringContextUtils的使用方式

--- src/main/java/org/tinycloud/tinyjob/utils/SpringContextUtils.java
+++ src/main/java/org/tinycloud/tinyjob/utils/SpringContextUtils.java
@@ -1,13 +1,17 @@
 package org.tinycloud.tinyjob.utils;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultSingletonBeanRegistry;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,85 +21,134 @@ import java.util.Map;
  * @version 2023-02-09 16:11
  */
 @Component
-public class SpringContextUtils implements ApplicationContextAware, DisposableBean {
-    public static ApplicationContext applicationContext;
+public class SpringContextUtils implements BeanFactoryPostProcessor, ApplicationContextAware {
+
+    /**
+     * "@PostConstruct"注解标记的类中，由于ApplicationContext还未加载，导致空指针<br>
+     * 因此实现BeanFactoryPostProcessor注入ConfigurableListableBeanFactory实现bean的操作
+     */
+    private static ConfigurableListableBeanFactory beanFactory;
+
+    /**
+     * Spring应用上下文环境
+     */
+    private static ApplicationContext applicationContext;
 
     private SpringContextUtils() {
         super();
     }
 
-    /**
-     * 实现ApplicationContextAware接口的context注入函数, 将其存入静态变量.
-     */
+    @SuppressWarnings("NullableProblems")
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        SpringContextUtils.beanFactory = beanFactory;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
         SpringContextUtils.applicationContext = applicationContext;
     }
 
     /**
-     * 取得存储在静态变量中的ApplicationContext.
+     * 获取{@link ApplicationContext}
+     *
+     * @return {@link ApplicationContext}
      */
     public static ApplicationContext getApplicationContext() {
-        checkApplicationContext();
         return applicationContext;
     }
 
     /**
-     * 从静态变量ApplicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     * 获取{@link ListableBeanFactory}，可能为{@link ConfigurableListableBeanFactory} 或 {@link ApplicationContextAware}
+     *
+     * @return {@link ListableBeanFactory}
+     * @since 5.7.0
+     */
+    public static ListableBeanFactory getBeanFactory() {
+        final ListableBeanFactory factory = (null == beanFactory) ? applicationContext : beanFactory;
+        if (null == factory) {
+            throw new IllegalStateException("No ConfigurableListableBeanFactory or ApplicationContext injected, maybe not in the Spring environment?");
+        }
+        return factory;
+    }
+
+    /**
+     * 获取{@link ConfigurableListableBeanFactory}
+     *
+     * @return {@link ConfigurableListableBeanFactory}
+     * @throws IllegalStateException 当上下文非ConfigurableListableBeanFactory抛出异常
+     */
+    public static ConfigurableListableBeanFactory getConfigurableBeanFactory() throws IllegalStateException {
+        final ConfigurableListableBeanFactory factory;
+        if (null != beanFactory) {
+            factory = beanFactory;
+        } else if (applicationContext instanceof ConfigurableApplicationContext) {
+            factory = ((ConfigurableApplicationContext) applicationContext).getBeanFactory();
+        } else {
+            throw new IllegalStateException("No ConfigurableListableBeanFactory from context!");
+        }
+        return factory;
+    }
+
+    //通过name获取 Bean.
+
+    /**
+     * 通过name获取 Bean
+     *
+     * @param <T>  Bean类型
+     * @param name Bean名称
+     * @return Bean
      */
     @SuppressWarnings("unchecked")
     public static <T> T getBean(String name) {
-        return (T) getApplicationContext().getBean(name);
+        return (T) getBeanFactory().getBean(name);
     }
 
     /**
-     * 从静态变量ApplicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     * 通过class获取Bean
+     *
+     * @param <T>   Bean类型
+     * @param clazz Bean类
+     * @return Bean对象
      */
     public static <T> T getBean(Class<T> clazz) {
-        return getApplicationContext().getBean(clazz);
-    }
-
-    public static <T> T getBean(String name, Class<T> requiredType) {
-        return getApplicationContext().getBean(name, requiredType);
-    }
-
-    public static boolean containsBean(String name) {
-        return getApplicationContext().containsBean(name);
-    }
-
-    public static boolean isSingleton(String name) {
-        return getApplicationContext().isSingleton(name);
-    }
-
-    public static Class<? extends Object> getType(String name) {
-        return getApplicationContext().getType(name);
+        return getBeanFactory().getBean(clazz);
     }
 
     /**
-     * 获取指定类型对应的所有Bean的列表
+     * 通过name,以及Clazz返回指定的Bean
      *
-     * @param clazz 类、接口
-     * @param <T>   Bean类型
-     * @return List<Bean>
+     * @param <T>   bean类型
+     * @param name  Bean名称
+     * @param clazz bean类型
+     * @return Bean对象
      */
-    public static <T> List<T> getBeanList(Class<T> clazz) {
-        String[] beanNames = getApplicationContext().getBeanNamesForType(clazz);
-        List<T> beanList = new ArrayList<>();
-        for (String beanName : beanNames) {
-            beanList.add(getApplicationContext().getBean(beanName, clazz));
-        }
-        return beanList;
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getBeanFactory().getBean(name, clazz);
     }
 
     /**
      * 获取指定类型对应的所有Bean，包括子类
      *
-     * @param <T>   Bean类型
-     * @param clazz 类、接口，null表示获取所有bean
+     * @param <T>  Bean类型
+     * @param type 类、接口，null表示获取所有bean
      * @return 类型对应的bean，key是bean注册的name，value是Bean
+     * @since 5.3.3
      */
-    public static <T> Map<String, T> getBeanMap(Class<T> clazz) {
-        return getApplicationContext().getBeansOfType(clazz);
+    public static <T> Map<String, T> getBeansOfType(Class<T> type) {
+        return getBeanFactory().getBeansOfType(type);
+    }
+
+    /**
+     * 获取指定类型对应的Bean名称，包括子类
+     *
+     * @param type 类、接口，null表示获取所有bean名称
+     * @return bean名称
+     * @since 5.3.3
+     */
+    public static String[] getBeanNamesForType(Class<?> type) {
+        return getBeanFactory().getBeanNamesForType(type);
     }
 
     /**
@@ -103,26 +156,141 @@ public class SpringContextUtils implements ApplicationContextAware, DisposableBe
      *
      * @param key 配置项key
      * @return 属性值
+     * @since 5.3.3
      */
     public static String getProperty(String key) {
-        return getApplicationContext().getEnvironment().getProperty(key);
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key);
     }
 
     /**
-     * 清除applicationContext静态变量
+     * 获取配置文件配置项的值
+     *
+     * @param key          配置项key
+     * @param defaultValue 默认值
+     * @return 属性值
+     * @since 5.8.24
      */
-    public static void cleanApplicationContext() {
-        applicationContext = null;
+    public static String getProperty(String key, String defaultValue) {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key, defaultValue);
     }
 
-    private static void checkApplicationContext() {
-        if (applicationContext == null) {
-            throw new IllegalStateException("applicaitonContext未注入，请检查");
+    /**
+     * 获取配置文件配置项的值
+     *
+     * @param <T>          属性值类型
+     * @param key          配置项key
+     * @param targetType   配置项类型
+     * @param defaultValue 默认值
+     * @return 属性值
+     * @since 5.8.24
+     */
+    public static <T> T getProperty(String key, Class<T> targetType, T defaultValue) {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getProperty(key, targetType, defaultValue);
+    }
+
+    /**
+     * 获取应用程序名称
+     *
+     * @return 应用程序名称
+     * @since 5.7.12
+     */
+    public static String getApplicationName() {
+        return getProperty("spring.application.name");
+    }
+
+    /**
+     * 获取当前的环境配置，无配置返回null
+     *
+     * @return 当前的环境配置
+     * @since 5.3.3
+     */
+    public static String[] getActiveProfiles() {
+        if (null == applicationContext) {
+            return null;
+        }
+        return applicationContext.getEnvironment().getActiveProfiles();
+    }
+
+    /**
+     * 获取当前的环境配置，当有多个环境配置时，只获取第一个
+     *
+     * @return 当前的环境配置
+     * @since 5.3.3
+     */
+    public static String getActiveProfile() {
+        final String[] activeProfiles = getActiveProfiles();
+        return !ObjectUtils.isEmpty(activeProfiles) ? activeProfiles[0] : null;
+    }
+
+    /**
+     * 动态向Spring注册Bean
+     * <p>
+     * 由{@link org.springframework.beans.factory.BeanFactory} 实现，通过工具开放API
+     * <p>
+     * 更新: shadow 2021-07-29 17:20:44 增加自动注入，修复注册bean无法反向注入的问题
+     *
+     * @param <T>      Bean类型
+     * @param beanName 名称
+     * @param bean     bean
+     * @author shadow
+     * @since 5.4.2
+     */
+    public static <T> void registerBean(String beanName, T bean) {
+        final ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        factory.autowireBean(bean);
+        factory.registerSingleton(beanName, bean);
+    }
+
+    /**
+     * 注销bean
+     * <p>
+     * 将Spring中的bean注销，请谨慎使用
+     *
+     * @param beanName bean名称
+     * @author shadow
+     * @since 5.7.7
+     */
+    public static void unregisterBean(String beanName) {
+        final ConfigurableListableBeanFactory factory = getConfigurableBeanFactory();
+        if (factory instanceof DefaultSingletonBeanRegistry) {
+            DefaultSingletonBeanRegistry registry = (DefaultSingletonBeanRegistry) factory;
+            registry.destroySingleton(beanName);
+        } else {
+            throw new IllegalStateException("Can not unregister bean, the factory is not a DefaultSingletonBeanRegistry!");
         }
     }
 
-    @Override
-    public void destroy() throws Exception {
-        SpringContextUtils.cleanApplicationContext();
+    /**
+     * 发布事件
+     *
+     * @param event 待发布的事件，事件必须是{@link ApplicationEvent}的子类
+     * @since 5.7.12
+     */
+    public static void publishEvent(ApplicationEvent event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
+    }
+
+    /**
+     * 发布事件
+     * Spring 4.2+ 版本事件可以不再是{@link ApplicationEvent}的子类
+     *
+     * @param event 待发布的事件
+     * @since 5.7.21
+     */
+    public static void publishEvent(Object event) {
+        if (null != applicationContext) {
+            applicationContext.publishEvent(event);
+        }
     }
 }
